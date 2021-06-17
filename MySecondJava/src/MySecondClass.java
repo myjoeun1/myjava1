aacd4fb74baf0008b62b92715ea63ed2e1ef5212
TJ <TJ@DESKTOP-0S1SB1F>
2021-06-17 22:01:49 +0900

    참조형까지 커밋

--- MySecondJava/src/MySecondClass.java
+++ MySecondJava/src/MySecondClass.java
@@ -17,6 +17,16 @@ public class MySecondClass {
 		int mynumber2 = 10;
 		
 		System.out.println("mynumber의 값은 "+mynumber);
+		
+		String name = "홍길동";
+		String name2 = null;
+		String name3 = "";
+		
+		System.out.println("name의 값은"+name);
+		System.out.println("name2의 값은"+name2);
+		System.out.println("name3의 값은"+name3);
+		
+		
 		System.out.println("mynumber2의 값은 "+mynumber2);
 		
 	}
