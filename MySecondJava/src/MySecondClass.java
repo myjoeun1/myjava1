f2c1e949a1a349e6c9e97526b23b16da2b599aae
TJ <TJ@DESKTOP-0S1SB1F>
2021-06-17 21:38:59 +0900

    처음 커밋 20210617

---
+++ MySecondJava/src/MySecondClass.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ * @author KIM
+ *
+ */
+public class MySecondClass {
+
+	/**
+	 * 
+	 * @param args String
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello World2");
+		System.out.println("Hello World3");
+		System.out.println("Hello World4");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
+	}
+
+}
+
+//-encoding UTF-8 -charset UTF-8
+
+
